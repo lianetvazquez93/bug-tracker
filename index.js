67cd320b81b084a815dbdef772588fc8babbb3d6
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2019-07-22 17:59:38 +0200

    Handle database error

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const mongoose = require('mongoose');
 let setupController = require('./controllers/setup');
 let apiRoutes = require('./routes/api');
 
-mongoose.connect(process.env.DB_URI, {useNewUrlParser: true});
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true}).catch(error => {
+    console.log(error.message);
+    process.exit(1);
+});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
